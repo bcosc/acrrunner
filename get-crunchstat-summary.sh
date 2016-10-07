3cca3176bd2f0a3f6850ace6afc2365a60d5dc61
bcosc <bcosc@bu.edu>
2016-10-07 15:42:46 +0000

    added get-crunchstat-summary.sh that creates html and txt summaries and copies them to local machine to view

---
+++ get-crunchstat-summary.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+source ~/cs1/bin/activate
+if [ ! -d ~/crunchstat_summaries/$1 ]; then
+  mkdir ~/crunchstat_summaries/$1
+fi
+
+~/arvados/tools/crunchstat-summary/bin/crunchstat-summary --job $1 --format html > ~/crunchstat_summaries/$1/$1.html
+~/arvados/tools/crunchstat-summary/bin/crunchstat-summary --job $1 --format text > ~/crunchstat_summaries/$1/$1.txt
+
+scp -r -P12345 ~/crunchstat_summaries/$1/ 127.0.0.1:/home/bcosc/crunchstat_summaries/

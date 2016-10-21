e5b6718a1ca7e98bdc5fb45494da59644d74f3a5
bcosc <bcosc@bu.edu>
2016-10-21 18:13:38 +0000

    pi-output-summary outputs file sizes for all files in a pipeline_instance

---
+++ pi-output-summary.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+from __future__ import division
+import sys
+import arvados
+import subprocess
+import os
+import math
+
+# Taken from http://stackoverflow.com/questions/1094841/reusable-library-to-get-human-readable-version-of-file-size
+def sizeof_fmt(num, suffix='B'):
+    for unit in ['','Ki','Mi','Gi','Ti','Pi','Ei','Zi']:
+        if abs(num) < 1024.0:
+            return "%3.1f%s%s" % (num, unit, suffix)
+        num /= 1024.0
+    return "%.1f%s%s" % (num, 'Yi', suffix)
+
+def main(args):
+  wf_uuid = args[0]
+  keep_mount = args[1]
+  wf = arvados.api('v1').pipeline_instances().list(filters=[["uuid","=",wf_uuid]]).execute()
+  job_uuids = []
+  job_dict = {}
+  job_list = wf['items'][0]['components'].keys()
+  print job_list
+  job_list.sort()
+  with open(wf_uuid+'-outputsizes.txt','w') as out:
+    out.write("job_name, file_name, filesize")
+    for job_name in job_list:
+      job_dict[job_name] = []
+      job_uuid = wf['items'][0]['components'][job_name]['job']['uuid']
+      job = arvados.api('v1').jobs().list(filters=[["uuid","=",job_uuid]]).execute()
+      output_pdh = job['items'][0]['output']
+      collection = arvados.collection.CollectionReader(output_pdh)
+      for file in collection:
+        stat = os.stat(os.path.join(keep_mount, output_pdh, file))
+        filesize = stat.st_size
+        out.write("%s, %s, %s\n" % (job_name, file, sizeof_fmt(filesize)))
+
+if __name__ == '__main__':
+  print main(sys.argv[1:])
